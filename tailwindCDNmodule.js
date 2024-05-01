37531df653db246b7564743593741797a7af02e8
meyirman-is-creator <sarsenbaymeyirman@gmail.com>
2024-05-01 18:42:49 +0600

    i finished videoLessonsPage.html

---
+++ tailwindCDNmodule.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'accent-red': '#BA2025',
+        'black-main': '#4F4F4F',
+        'icon-linear': '#CB4045',
+        'english-color':'#e52127',
+        'math-color':'#50b521',
+        'blue-ielts':'#7A48E4',
+        'more-color':'#26BBEA',
+        // ... добавьте все другие кастомные цвета
+      },
+      borderRadius: {
+        'sm': '5px',
+        'md': '10px',
+        'lg': '50px',
+        'full': '50%',
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+      },
+      fontFamily: {
+        'qanelas': ['"Qanelas"', 'sans-serif'],
+      },
+    },
+  },
+};
